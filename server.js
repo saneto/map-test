b462a0398f27d59134b07e940748652b38a84f98
saneto <aymanezaircha@gmail.com>
2024-04-04 22:08:03 +0200

    fixe

---
+++ server.js
@@ -0,0 +1,37 @@
+/* server.js in root directory */
+const fs = require('fs');
+const path = require('path');
+
+const dir = "src/environments";
+const file = "environment.ts";
+
+const content = `${env.FIREBASE_VALUE}`;
+
+fs.access(dir, fs.constants.F_OK, (err) => {
+    if (err) {
+        // Directory doesn't exist
+        console.log("src doesn't exist, creating now", process.cwd());
+        // Create /src
+        try {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        catch (error) {
+            console.log(`Error while creating ${dir}. Error is ${error}`);
+            process.exit(1);
+        }
+    }
+    // Now write to file
+    try {
+        fs.writeFileSync(dir + "/" + file, content);
+        console.log("Created successfully in", process.cwd());
+        if (fs.existsSync(dir + "/" + file)) {
+            console.log("File is created", path.resolve(dir + "/" + file));
+            const str = fs.readFileSync(dir + "/" + file).toString();
+            console.log(str);
+            console.log(`${env.FIREBASE_VALUE}`.toString());
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+});
